cc6721ed2e7f7aaf7c2eac88bfe34532e1b83978
helloghostt <lamar0610@gmail.com>
2024-09-02 18:08:54 +0900

    eslint-2

--- src/App.tsx
+++ src/App.tsx
@@ -16,15 +16,15 @@ import MyPage from "./pages/mypage/MyPage";
 import LoginModal from "./components/LoginModal";
 import AboutPage from "./pages/about/AboutPage";
 
-let API_URL;
+// let API_URL;
 
-if (process.env.NODE_ENV === "production") {
-  API_URL = process.env.NODE_ENV_PROD;
-} else if (process.env.NODE_ENV === "development") {
-  API_URL = process.env.NODE_ENV_DEV;
-} else {
-  API_URL = process.env.NODE_ENV;
-}
+// if (process.env.NODE_ENV === "production") {
+//   API_URL = process.env.NODE_ENV_PROD;
+// } else if (process.env.NODE_ENV === "development") {
+//   API_URL = process.env.NODE_ENV_DEV;
+// } else {
+//   API_URL = process.env.NODE_ENV;
+// }
 
 const ChatBoardWrapper = () => {
   const { roomId } = useParams<{ roomId: string }>();
